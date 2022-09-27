dfb179b253c1f9019b178c1183384fe7c715cd19
Cal Mense <104301991+calmense@users.noreply.github.com>
2022-09-27 08:18:59 +0200

    Update schraubenbemessung_ec5_87.py

--- 02_Schraubenbemessung_ec5_87/schraubenbemessung_ec5_87.py
+++ 02_Schraubenbemessung_ec5_87/schraubenbemessung_ec5_87.py
@@ -25,7 +25,7 @@ hide_st_style = """
             <style>
             MainMenu {visibility: hidden;}
             #footer {visibility: hidden;}
-            #header {visibility: hidden;}
+            header {visibility: hidden;}
             </style>
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
